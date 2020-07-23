90b326d05275bd774f35f66a714b0872508e1760
comfort museo <you@example.com>
2020-07-23 16:54:04 +0300

    function 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-var D = date
+function getDate () {
+    var dd= Date();
+
+    var  day = parseInt(document.getElementById("day").value);
+    var  month = parseInt(document.getElementById("month").value);
+    var  year = parseInt(document.getElementById("year").value);
+    
+    if(day <0 || >31) {
+        document.getElementsByClassName("day error").innerHtml = "invalid";
+        return false;
+    } else {
+        document.setDate(date);
+    }
+
+    if(month <0 || >12) {
+        document.getElementsByClassName("month error").innerHtml = "invalid";
+        return false;
+    } else {
+        document.setMonth(month);
+    }
+
+    if(year <0 || >2020) {
+        document.getElementsByClassName("year error").innerHtml = "invalid";
+        return false;
+    } else {
+        document.setYear(year);
+    }
+
+function akanName () {
+    
+}
+akanName()
+
+
+
